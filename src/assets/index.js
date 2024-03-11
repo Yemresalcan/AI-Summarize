59b55f207c3fb6e89822e52f8d8cddc8be769d11
Yemresalcan <yunusemresalcan@gmail.com>
2024-03-12 01:05:11 +0300

    initial comit and update to project

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import copy2 from './copy2.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    copy2
+}
